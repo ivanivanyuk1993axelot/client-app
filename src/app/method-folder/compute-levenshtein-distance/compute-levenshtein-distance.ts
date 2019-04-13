04cb6c4dc8851dce7a94d2e184bc9e40aa848968
i.ivanyuk <i.ivanyuk@axelot.ru>
2019-04-13 21:31:57 +0300

    Преобразовал алгоритм в более эффективный, с потреблением памяти O(n), а не O(m*n)

--- src/app/method-folder/compute-levenshtein-distance/compute-levenshtein-distance.ts
+++ src/app/method-folder/compute-levenshtein-distance/compute-levenshtein-distance.ts
@@ -13,28 +13,17 @@ export function computeLevenshteinDistance(leftString: string, rightString: stri
     return longerString.length;
   }
 
-  const matrix = [];
-
-  // increment along the first column of each row
-  let longerStringIndex;
-  for (longerStringIndex = 0; longerStringIndex <= longerString.length; longerStringIndex++) {
-    matrix[longerStringIndex] = [longerStringIndex];
-  }
-
-  // increment each column in the first row
   let shorterStringIndex;
-  for (shorterStringIndex = 0; shorterStringIndex <= shorterString.length; shorterStringIndex++) {
-    matrix[0][shorterStringIndex] = shorterStringIndex;
+  let costList = new Array<number>(shorterString.length + 1);
+  let newCostList = new Array<number>(shorterString.length + 1);
+  for (shorterStringIndex = 0; shorterStringIndex < shorterString.length + 1; shorterStringIndex++) {
+    costList[shorterStringIndex] = shorterStringIndex;
   }
 
   // Fill in the rest of the matrix
-  let costList;
-  let newCostList;
-  for (longerStringIndex = 1; longerStringIndex < longerString.length + 1; longerStringIndex++) {
-    newCostList = matrix[longerStringIndex];
+  for (let longerStringIndex = 1; longerStringIndex < longerString.length + 1; longerStringIndex++) {
     newCostList[0] = longerStringIndex;
     for (shorterStringIndex = 1; shorterStringIndex < shorterString.length + 1; shorterStringIndex++) {
-      costList = matrix[longerStringIndex - 1];
       newCostList[shorterStringIndex] = Math.min(
         costList[shorterStringIndex - 1] + (longerString[longerStringIndex - 1] !== shorterString[shorterStringIndex - 1] ? 1 : 0), // substitution
         costList[shorterStringIndex] + 1, // deletion
