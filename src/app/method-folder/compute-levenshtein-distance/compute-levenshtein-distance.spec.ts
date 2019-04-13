b9ff00ad167d37121451214e0fbce3e706a5a28f
i.ivanyuk <i.ivanyuk@axelot.ru>
2019-04-13 17:37:20 +0300

    Улучшил потребление памяти алгоритмом

---
+++ src/app/method-folder/compute-levenshtein-distance/compute-levenshtein-distance.spec.ts
@@ -0,0 +1,12 @@
+import {computeLevenshteinDistance} from './compute-levenshtein-distance';
+import {TestBed} from '@angular/core/testing';
+
+describe('MenuService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('computeLevenshteinDistance', () => {
+    expect(computeLevenshteinDistance('sitting', 'kitten')).toBe(3);
+    expect(computeLevenshteinDistance('saturday', 'sunday')).toBe(3);
+    expect(computeLevenshteinDistance('day', 'a')).toBe(2);
+  });
+});
--- src/app/method-folder/compute-levenshtein-distance/compute-levenshtein-distance.ts
+++ src/app/method-folder/compute-levenshtein-distance/compute-levenshtein-distance.ts
@@ -2,26 +2,34 @@ export function computeLevenshteinDistance(
   a: string,
   b: string,
 ): number {
-  const aLen = a.length;
-  const bLen = b.length;
-  let previousDistanceList = new Array<number>(bLen + 1);
-  let currentDistanceList = new Array<number>(bLen + 1);
+  let shorterString: string;
+  let longerString: string;
+  if (a.length < b.length) {
+    [shorterString, longerString] = [a, b];
+  } else {
+    [shorterString, longerString] = [b, a];
+  }
 
-  for (let i = 0; i < bLen; i++) {
+  const longerStringLength = longerString.length;
+  const shorterStringLength = shorterString.length;
+  let previousDistanceList = new Array<number>(shorterStringLength + 1);
+  let currentDistanceList = new Array<number>(shorterStringLength + 1);
+
+  for (let i = 0; i < shorterStringLength; i++) {
     previousDistanceList[i] = i;
   }
 
-  for (let i = 0; i < aLen - 1; i++) {
+  for (let i = 0; i < longerStringLength - 1; i++) {
     currentDistanceList[0] = i + 1;
-    for (let j = 0; j < bLen - 1; j++) {
+    for (let j = 0; j < shorterStringLength - 1; j++) {
       const deletionCost = previousDistanceList[j + 1] + 1;
       const insertionCost = currentDistanceList[j] + 1;
-      const substitutionCost = a[i] === b[j] ? previousDistanceList[j] : previousDistanceList[j] + 1;
+      const substitutionCost = longerString[i] === shorterString[j] ? previousDistanceList[j] : previousDistanceList[j] + 1;
 
       currentDistanceList[j + 1] = Math.min(deletionCost, insertionCost, substitutionCost);
     }
     [previousDistanceList, currentDistanceList] = [currentDistanceList, previousDistanceList];
   }
 
-  return previousDistanceList[bLen - 1];
+  return previousDistanceList[shorterStringLength - 1];
 }
